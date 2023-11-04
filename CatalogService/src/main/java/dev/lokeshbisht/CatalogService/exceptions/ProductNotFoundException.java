7ed51dce7d58f905a90ea621172883425d136a1b
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-11-04 16:46:33 +0530

    Add product not found exception and global exception handler

---
+++ CatalogService/src/main/java/dev/lokeshbisht/CatalogService/exceptions/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.CatalogService.exceptions;
+
+import java.io.Serial;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
