dc3b1da0e9cadb565e2d0b5dc0a9319306c400de
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-11-05 23:36:18 +0530

    feat: #22

--- CatalogService/src/main/java/dev/lokeshbisht/CatalogService/repository/ProductRepository.java
+++ CatalogService/src/main/java/dev/lokeshbisht/CatalogService/repository/ProductRepository.java
@@ -4,4 +4,6 @@ import dev.lokeshbisht.CatalogService.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Product findByProductCode(String productCode);
 }
