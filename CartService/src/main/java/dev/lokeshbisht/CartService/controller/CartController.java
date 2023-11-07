e51d1b6a807f4cc1bab20518a320e6d48b4022d1
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-11-08 00:01:18 +0530

    Add catalog service circuit breaker

--- CartService/src/main/java/dev/lokeshbisht/CartService/controller/CartController.java
+++ CartService/src/main/java/dev/lokeshbisht/CartService/controller/CartController.java
@@ -1,9 +1,12 @@
 package dev.lokeshbisht.CartService.controller;
 
+import dev.lokeshbisht.CartService.constants.MessageTemplates;
 import dev.lokeshbisht.CartService.dto.ApiResponseDto;
+import dev.lokeshbisht.CartService.dto.MetaDataDto;
 import dev.lokeshbisht.CartService.dto.cart.CartDto;
 import dev.lokeshbisht.CartService.model.Cart;
 import dev.lokeshbisht.CartService.service.CartService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +28,14 @@ public class CartController {
     }
 
     @GetMapping("/cart/{cartId}")
+    @CircuitBreaker(name = "catalogServiceBreaker", fallbackMethod = "catalogFallback")
     public ApiResponseDto<Cart> getCart(@PathVariable String cartId) {
         return cartService.getCartByCartId(cartId);
+    }
+
+    public ApiResponseDto<Cart> catalogFallback(String cartId, Exception ex) {
+        logger.error("Executing catalog fallback because catalog service is down: {}", ex.getMessage());
+        MetaDataDto metaDataDto = MetaDataDto.builder().build();
+        return new ApiResponseDto<Cart>(null, "OK",  new String []{MessageTemplates.CATALOG_SERVICE_DOWN}, metaDataDto);
     }
 }
