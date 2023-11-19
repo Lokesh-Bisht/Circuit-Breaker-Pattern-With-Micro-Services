c5e66792bf5ecc0f05208198d33efd259d98552a
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-11-19 23:59:44 +0530

    Add license

--- CartService/src/main/java/dev/lokeshbisht/CartService/mapper/CartMapper.java
+++ CartService/src/main/java/dev/lokeshbisht/CartService/mapper/CartMapper.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2023 Lokesh Bisht
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package dev.lokeshbisht.CartService.mapper;
 
 import dev.lokeshbisht.CartService.dto.cart.CartDto;
